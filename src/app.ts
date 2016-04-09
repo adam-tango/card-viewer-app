1b7c3cd910a8ee2c03e9725906140d92f087f2d2
adam.tango <adam.tango@knights.ucf.edu>
2016-04-08 23:16:41 -0400

    initial seed project

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import {enableProdMode} from 'angular2/core';
+enableProdMode();
+
+import {bootstrap} from 'angular2/platform/browser';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+
+import {SeedApp} from './app/seed-app';
+
+
+
+bootstrap(SeedApp, [HTTP_PROVIDERS, ROUTER_PROVIDERS])
+.catch(err => console.error(err));
